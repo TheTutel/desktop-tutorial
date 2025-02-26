93080bab38487422fb1bd5a8d680a6f27478b6d5
TheTutel <timofey.achieve@gmail.com>
2025-02-26 19:58:43 +0200

    da

---
+++ chrgr.py
@@ -0,0 +1,7 @@
+charge_level=int(input())
+if charge_level>=20:
+    print("everything is okay, nothing to worry about😐")
+elif charge_level==0:
+    print("bruh, ur ded💀")
+elif charge_level<20:
+    print("something goes wrong")
---
+++ rt.pornhub.com.py
@@ -0,0 +1,8 @@
+print("сколько")
+age=int(input())
+if age<18:
+    print("Нельзя")
+elif age>=18:
+    print("Можно")
+else:
+    print("С Альфа-Центавры не принимаем")
